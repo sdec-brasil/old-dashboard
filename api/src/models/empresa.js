ac7bb900c92270c2ee0dfa73398585559aeff430
Tiago Loriato <tloriatosimoes@gmail.com>
2019-03-03 12:21:02 -0300

    api: fixing empresa model

--- api/src/models/empresa.js
+++ api/src/models/empresa.js
@@ -1,73 +1,60 @@
 // Empresa
 export default (sequelize, DataTypes) => sequelize.define('empresa', {
   cnpj: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(14),
     primaryKey: true,
     unique: true,
     allowNull: false,
   },
   razao_social: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(150),
     unique: true,
     allowNull: false,
   },
   nome_fantasia: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(60),
     allowNull: false,
   },
   endereco_empresa: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(125),
     allowNull: false,
   },
   numero_endereco: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.INTEGER(10),
     allowNull: false,
   },
   complemento_endereco: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(60),
+    allowNull: true,
   },
   bairro_endereco: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(60),
     allowNull: false,
   },
   cidade_endereco: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(7),
     allowNull: false,
   },
   unidade_federacao: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(2),
     allowNull: false,
   },
   pais_endereco: {
-    type: DataTypes.TEXT,
+    type: DataTypes.TEXT(4),
+    allowNull: true,
   },
   cep: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(8),
     allowNull: false,
   },
   email: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    type: DataTypes.STRING(80),
+    allowNull: true,
   },
   telefone: {
-    type: DataTypes.STRING,
-    allowNull: false, // talvez possa ser null, not sure
+    type: DataTypes.STRING(20),
+    allowNull: true,
   },
-  id_transacao_de_registro: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    // tem que fazer a ligacao com a tabela transferencias
-  },
-  id_bloco_de_registro: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    // fazer a ligacao com a tabela bloco
-  },
-  endereco_blockchain: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    // fazer a ligacao com a tabela endereco_blockchain
-  },
+}, {
+  underscored: true,
 });
