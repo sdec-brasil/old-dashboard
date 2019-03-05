fb117c247774ac017dbc1b66fe054c162008a3eb
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-03-05 16:22:20 -0300

    API: Adicionando algumas relacoes entre os models, ainda nao esta finalizado

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -7,7 +7,42 @@ import databaseConnection from '../setup/databaseConnection';
 const models = {
   Thought: databaseConnection.import('./thought'),
   Bloco: databaseConnection.import('./bloco'),
+  Boleto: databaseConnection.import('./boleto'),
+  Dados_Bancarios: databaseConnection.import('./dados_bancarios'),
+  Empresa: databaseConnection.import('./empresa'),
+  Endereco_Blockchain: databaseConnection.import('./endereco_blockchain'),
+  Nota_Fiscal: databaseConnection.import('./nota_fiscal'),
+  Prefeitura: databaseConnection.import('./prefeitura'),
+  Transferencia: databaseConnection.import('./transferencia'),
 };
+
+// boleto
+models.Boleto.hasMany(models.Nota_Fiscal, { foreignKey: 'id_notas', constraints: true });
+models.Boleto.hasOne(models.Empresa, { foreignKey: 'cnpj', constraints: true });
+/* Tenho que ver como fazer o inverso, porque quando a nota eh criada, ela ainda nao pertence
+ a um boleto, somente quando o boleto eh criado, entao nao sei se faz sentido dizer,
+   notaFiscal.belongsTo( Boleto , ... ) */
+
+// empresa
+models.Empresa.hasOne(models.Transferencia, { foreignKey: 'id_transferencia' });
+
+// nota_fiscal
+models.Nota_Fiscal.hasOne(models.Prefeitura, { foreignKey: 'codigo_prefeitura' });
+models.Nota_Fiscal.hasOne(models.Empresa, { foreignKey: 'cnpj' });
+models.Nota_Fiscal.hasOne(models.Transferencia, { foreignKey: 'id_nota_fiscal' });
+models.Nota_Fiscal.hasOne(models.Bloco, { foreignKey: 'id_bloco' });
+//
+
+// transferencia
+models.Transferencia.hasOne(models.Bloco, { foreignKey: 'id_bloco', constraints: true });
+models.Transferencia.hasOne(models.Endereco_Blockchain, {
+  foreignKey: 'endereco_remetente',
+  constraints: true,
+});
+models.Transferencia.hasOne(models.Endereco_Blockchain, {
+  foreignKey: 'endereco_destinatario',
+  constraints: true,
+});
 
 Object.keys(models).forEach((modelName) => {
   if ('associate' in models[modelName]) {
