631a1ae3046878ac2cf7b3e85949bda4aaaf01c6
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-05 18:04:57 -0300

    API: Removing and Fixing Legacy Code

--- api/src/schema/mutation.js
+++ api/src/schema/mutation.js
@@ -1,16 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql';
 
-// App Imports
-import * as thought from './thoughts/fields/mutations';
-
 // Mutation
 const mutation = new GraphQLObjectType({
   name: 'mutations',
   description: '...',
 
   fields: {
-    ...thought,
+
   },
 });
 
--- api/src/schema/query.js
+++ api/src/schema/query.js
@@ -1,18 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql';
 
-// App Imports
-import * as thought from './thoughts/fields/query';
-import * as block from './blocos/fields/query';
-import * as boleto from './boletos/fields/query';
-
 // Query
 const query = new GraphQLObjectType({
   name: 'query',
   description: '...',
 
   fields: () => ({
-    ...thought, ...block, ...boleto,
+
   }),
 });
 
