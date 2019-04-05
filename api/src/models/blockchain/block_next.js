80b3b287837521b15759bd11a7fd1dc1d6d4fc59
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-05 18:58:16 -0300

    Fixes #16

--- api/src/models/blockchain/block_next.js
+++ api/src/models/blockchain/block_next.js
@@ -1,41 +1,24 @@
 export default function (sequelize, DataTypes) {
-  const Block_Next = sequelize.define('Block_Next', {
-    block_next_uuid: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      unique: true,
-    },
+  return sequelize.define('block_next', {
     block_id: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(14, 0),
       allowNull: false,
+      primaryKey: true,
       references: {
         model: 'block',
         key: 'block_id',
       },
     },
     next_block_id: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(14, 0),
       allowNull: false,
+      primaryKey: true,
       references: {
         model: 'block',
         key: 'block_id',
       },
     },
   }, {
-    underscored: true,
     tableName: 'block_next',
-    indexes:
-    [
-      {
-        unique: true,
-        fields: ['block_id', 'next_block_id'],
-        allowNull: false,
-      },
-    ],
   });
-
-  Block_Next.associate = (models) => {
-    Block_Next.belongsTo(models.block, { as: 'next_block', foreignKey: 'next_block_id' });
-    Block_Next.belongsTo(models.block, { as: 'block', foreignKey: 'block_id' });
-  };
 }
