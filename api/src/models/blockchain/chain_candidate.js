048e4cf345fbb801e7627d84297b42ef92bd9aae
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-05 19:03:32 -0300

    Partial fix to #20

--- api/src/models/blockchain/chain_candidate.js
+++ api/src/models/blockchain/chain_candidate.js
@@ -1,43 +1,28 @@
 export default function (sequelize, DataTypes) {
-  const Chain_Candidate = sequelize.define('Chain_Candidate', {
-    chain_uuid: {
-      type: DataTypes.UUID,
+  return sequelize.define('chain_candidate', {
+    chain_id: {
+      type: DataTypes.DECIMAL(10, 0),
       allowNull: false,
       primaryKey: true,
     },
-    chain_id: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
     block_id: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(14, 0),
       allowNull: false,
+      primaryKey: true,
       references: {
         model: 'block',
         key: 'block_id',
       },
     },
     in_longest: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(1, 0),
       allowNull: true,
     },
     block_height: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(14, 0),
       allowNull: true,
     },
   }, {
-    underscored: true,
     tableName: 'chain_candidate',
-    indexes: [
-      {
-        unique: true,
-        fields: ['chain_id', 'block_id'],
-        allowNull: false,
-      },
-    ],
   });
-
-  Chain_Candidate.associate = (models) => {
-    Chain_Candidate.belongsTo(models.block, { foreignKey: { name: 'block_id', allowNull: false } });
-  };
 }
