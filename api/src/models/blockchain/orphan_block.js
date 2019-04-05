6d33f4273d32c8b69b927eff3149ab4eff16bce6
Tiago Loriato <tloriatosimoes@gmail.com>
2019-04-05 19:11:15 -0300

    Partial fix to #24

--- api/src/models/blockchain/orphan_block.js
+++ api/src/models/blockchain/orphan_block.js
@@ -1,25 +1,19 @@
 export default function (sequelize, DataTypes) {
-  const Orphan_Block = sequelize.define('orphan_block', {
+  return sequelize.define('orphan_block', {
     block_id: {
-      type: DataTypes.DOUBLE,
+      type: DataTypes.DECIMAL(14, 0),
       allowNull: false,
+      primaryKey: true,
       references: {
         model: 'block',
         key: 'block_id',
       },
     },
     block_hashPrev: {
-      type: DataTypes.BLOB,
+      type: 'BINARY(32)',
       allowNull: false,
     },
   }, {
-    underscored: true,
     tableName: 'orphan_block',
   });
-
-  Orphan_Block.associate = (models) => {
-    Orphan_Block.belongsTo(models.block, { foreignKey: 'block_id' });
-  };
-
-  return Orphan_Block;
 }
