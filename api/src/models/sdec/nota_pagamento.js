79ae2947b9772e9878686183941f9593f29b764a
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-04-16 10:57:59 -0300

    Removing tricky associations

--- api/src/models/sdec/nota_pagamento.js
+++ api/src/models/sdec/nota_pagamento.js
@@ -26,11 +26,11 @@ export default function (sequelize, DataTypes) {
     freezeTableName: true,
   });
 
-  nota_pagamento.associate = (models) => {
-    nota_pagamento.belongsTo(models.nota_fiscal, { primaryKey: { name: 'id_nota', allowNull: false } });
-    nota_pagamento.belongsTo(models.empresa, { primaryKey: { name: 'cnpj_empresa', allowNull: false } });
-    nota_pagamento.belongsTo(models.metodo_pagamento, { primaryKey: { name: 'id_metodo', allowNull: false } });
-  };
+  /*nota_pagamento.associate = (models) => {
+    // nota_pagamento.belongsTo(models.nota_fiscal, { primaryKey: { name: 'id_nota', allowNull: false } });
+    // nota_pagamento.belongsTo(models.empresa, { primaryKey: { name: 'cnpj_empresa', allowNull: false } });
+    // nota_pagamento.belongsTo(models.metodo_pagamento, { primaryKey: { name: 'id_metodo', allowNull: false } });
+  };*/
 
   return nota_pagamento;
 }
