35875a00776a0cad02ab96e4d02c52804c9b6268
Tiago Loriato <tloriatosimoes@gmail.com>
2019-06-04 14:34:12 -0300

    Jest working

--- api/src/setup/loadModules.js
+++ api/src/setup/loadModules.js
@@ -75,8 +75,10 @@ export default function (server) {
 
   // From time to time we need to clean up any expired tokens
   // in the database
-  setInterval(() => {
-    accessTokens.removeExpired()
-      .catch(err => console.error('Error trying to remove expired tokens:', err.stack));
-  }, db.timeToCheckExpiredTokens * 1000);
+  if (process.env.NODE_ENV !== 'test') {
+    setInterval(() => {
+      accessTokens.removeExpired()
+        .catch(err => console.error('Error trying to remove expired tokens:', err.stack));
+    }, db.timeToCheckExpiredTokens * 1000);
+  }
 }
--- api/src/tests/index.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable func-names */
-/* eslint-disable no-undef */
-import invoice from './invoices/index.test';
-
-describe('Testing API Endpoints', function () {
-  describe('Invoice Endpoints', invoice.bind(this));
-});
