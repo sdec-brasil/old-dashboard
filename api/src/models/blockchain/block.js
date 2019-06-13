47201abaa10ff25bc2b72d4e085282f995fc200c
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-06-13 10:51:57 -0300

    Adding datetime field to block model

--- api/src/models/blockchain/block.js
+++ api/src/models/blockchain/block.js
@@ -1,92 +1,100 @@
 export default function (sequelize, DataTypes) {
-  return sequelize.define('block', {
-    block_id: {
-      type: DataTypes.DECIMAL(14, 0),
-      allowNull: false,
-      primaryKey: true,
-    },
-    block_hash: {
-      type: 'BINARY(32)',
-      allowNull: false,
-      unique: true,
-    },
-    block_version: {
-      type: DataTypes.DECIMAL(10, 0),
-      allowNull: true,
-    },
-    block_hashMerkleRoot: {
-      type: 'BINARY(32)',
-      allowNull: true,
-    },
-    block_nTime: {
-      type: DataTypes.DECIMAL(20, 0),
-      allowNull: true,
-    },
-    block_nBits: {
-      type: DataTypes.DECIMAL(10, 0),
-      allowNull: true,
-    },
-    block_nNonce: {
-      type: DataTypes.DECIMAL(10, 0),
-      allowNull: true,
-    },
-    block_height: {
-      type: DataTypes.DECIMAL(14, 0),
-      allowNull: true,
-    },
-    prev_block_id: {
-      type: DataTypes.DECIMAL(14, 0),
-      allowNull: true,
-      references: {
-        model: 'block',
-        key: 'block_id',
+  return sequelize.define(
+    'block',
+    {
+      block_id: {
+        type: DataTypes.DECIMAL(14, 0),
+        allowNull: false,
+        primaryKey: true,
+      },
+      block_hash: {
+        type: 'BINARY(32)',
+        allowNull: false,
+        unique: true,
+      },
+      block_version: {
+        type: DataTypes.DECIMAL(10, 0),
+        allowNull: true,
+      },
+      block_hashMerkleRoot: {
+        type: 'BINARY(32)',
+        allowNull: true,
+      },
+      block_nTime: {
+        type: DataTypes.DECIMAL(20, 0),
+        allowNull: false,
+      },
+      block_datetime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      block_nBits: {
+        type: DataTypes.DECIMAL(10, 0),
+        allowNull: true,
+      },
+      block_nNonce: {
+        type: DataTypes.DECIMAL(10, 0),
+        allowNull: true,
+      },
+      block_height: {
+        type: DataTypes.DECIMAL(14, 0),
+        allowNull: true,
+      },
+      prev_block_id: {
+        type: DataTypes.DECIMAL(14, 0),
+        allowNull: true,
+        references: {
+          model: 'block',
+          key: 'block_id',
+        },
+      },
+      search_block_id: {
+        type: DataTypes.DECIMAL(14, 0),
+        allowNull: true,
+        references: {
+          model: 'block',
+          key: 'block_id',
+        },
+      },
+      block_chain_work: {
+        type: 'BINARY(38)',
+        allowNull: true,
+      },
+      block_value_in: {
+        type: DataTypes.DECIMAL(30, 0),
+        allowNull: true,
+      },
+      block_value_out: {
+        type: DataTypes.DECIMAL(30, 0),
+        allowNull: true,
+      },
+      block_total_satoshis: {
+        type: DataTypes.DECIMAL(26, 0),
+        allowNull: true,
+      },
+      block_total_seconds: {
+        type: DataTypes.DECIMAL(20, 0),
+        allowNull: true,
+      },
+      block_satoshi_seconds: {
+        type: DataTypes.DECIMAL(28, 0),
+        allowNull: true,
+      },
+      block_total_ss: {
+        type: DataTypes.DECIMAL(28, 0),
+        allowNull: true,
+      },
+      block_num_tx: {
+        type: DataTypes.DECIMAL(10, 0),
+        allowNull: false,
+      },
+      block_ss_destroyed: {
+        type: DataTypes.DECIMAL(28, 0),
+        allowNull: true,
       },
     },
-    search_block_id: {
-      type: DataTypes.DECIMAL(14, 0),
-      allowNull: true,
-      references: {
-        model: 'block',
-        key: 'block_id',
-      },
+    {
+      tableName: 'block',
     },
-    block_chain_work: {
-      type: 'BINARY(38)',
-      allowNull: true,
-    },
-    block_value_in: {
-      type: DataTypes.DECIMAL(30, 0),
-      allowNull: true,
-    },
-    block_value_out: {
-      type: DataTypes.DECIMAL(30, 0),
-      allowNull: true,
-    },
-    block_total_satoshis: {
-      type: DataTypes.DECIMAL(26, 0),
-      allowNull: true,
-    },
-    block_total_seconds: {
-      type: DataTypes.DECIMAL(20, 0),
-      allowNull: true,
-    },
-    block_satoshi_seconds: {
-      type: DataTypes.DECIMAL(28, 0),
-      allowNull: true,
-    },
-    block_total_ss: {
-      type: DataTypes.DECIMAL(28, 0),
-      allowNull: true,
-    },
-    block_num_tx: {
-      type: DataTypes.DECIMAL(10, 0),
-      allowNull: false,
-    },
-    block_ss_destroyed: {
-      type: DataTypes.DECIMAL(28, 0),
-      allowNull: true,
-    },
-  }, {
-    tableName: 'block',
-  });
+  );
 }
