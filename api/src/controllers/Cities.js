6fe7834fa542a90de5e62df190ddf53569d0c8ad
David Beyda <dbeyda@globo.com>
2019-06-13 20:07:16 -0300

    Formatting response of cities endpoint

--- api/src/controllers/Cities.js
+++ api/src/controllers/Cities.js
@@ -2,6 +2,8 @@
 
 import models from '../models';
 import { limitSettings } from '../config/config';
+import { treatNestedFilters } from '../utils';
+import ResponseList from '../utils/response';
 
 
 const sqs = require('sequelize-querystring');
@@ -9,17 +11,28 @@ const sqs = require('sequelize-querystring');
 export default class CitiesController {
   async get(req, res) {
     const sq = sqs.withSymbolicOps(models.Sequelize, { symbolic: true });
+    const where = req.query.filter ? sq.find(req.query.filter) : {};
+    treatNestedFilters(req.query, where);
     models.prefeitura.findAndCountAll({
       offset: parseInt(req.query.offset, 10) || 0,
       limit: parseInt(req.query.limit, 10) || limitSettings.city.get,
-      where: req.query.filter ? sq.find(req.query.filter) : {},
+      where,
       order: req.query.sort ? sq.sort(req.query.sort) : [],
+      include: [
+        {
+          model: models.municipio,
+          include: [
+            models.estado, models.regiao,
+          ],
+        },
+      ],
     }).then((results) => {
-      res.json(results);
+      const response = new ResponseList(req, results);
+      res.status(200).json(response.value());
     }).catch((err) => {
-      res.json(err.message);
+      res.status(500).json({ error: err.message });
+      throw err;
     });
-    // throw new Error('Not implemented');
   }
 
   async getById(req, res) {
