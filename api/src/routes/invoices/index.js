ba992157c273088b2c32455a6eb67ebda40eefa5
David Beyda <dbeyda@globo.com>
2019-07-04 22:55:33 -0300

    Setting Invoice field validations and optional fields

--- api/src/routes/invoices/index.js
+++ api/src/routes/invoices/index.js
@@ -12,8 +12,6 @@ export default {
     path: 'Invoices.post',
     middlewares: [
       // login.ensureLoggedIn,
-      body('prestacao')
-        .exists(),
       body('prestacao.prefeituraIncidencia', 'valor nao valido').isNumeric()
         .isLength({ min: 7, max: 7 })
         .custom(async (value, { req }) => {
@@ -59,88 +57,91 @@ export default {
         }),
       body('prestacao.competencia').isInt(),
       body('prestacao.valServicos').isInt(),
-      body('prestacao.valDeducoes').isInt(),
-      body('prestacao.valPis').isInt(),
-      body('prestacao.valCofins').isInt(),
-      body('prestacao.valInss').isInt(),
-      body('prestacao.valIr').isInt(),
-      body('prestacao.valCsll').isInt(),
-      body('prestacao.outrasRetencoes').isInt(),
-      body('prestacao.valTotalTributos').isInt(),
+      body('prestacao.valDeducoes').isInt().optional(),
+      body('prestacao.valPis').isInt().optional(),
+      body('prestacao.valCofins').isInt().optional(),
+      body('prestacao.valInss').isInt().optional(),
+      body('prestacao.valIr').isInt().optional(),
+      body('prestacao.valCsll').isInt().optional(),
+      body('prestacao.outrasRetencoes').isInt().optional(),
+      body('prestacao.valTotalTributos').isInt().optional(),
       body('prestacao.valIss').isInt(),
-      body('prestacao.descontoIncond').isInt(),
-      body('prestacao.descontoCond').isInt(),
+      body('prestacao.descontoIncond').isInt().optional(),
+      body('prestacao.descontoCond').isInt().optional(),
       body('prestacao.issRetido').isBoolean(),
       body('prestacao.respRetencao')
         .custom((value, { req }) => {
           if (req.body.prestacao.issRetido
            && req.body.prestacao.issRetido === true) {
-            console.log('bati no 1');
-            if (!([1, 2].includes(value))) {
+            console.log('bati no 1', typeof (value));
+            if (value !== 1 && value !== 2) {
               throw new Error('respRetencao deve ser 1 ou 2 (inteiro)');
             }
           }
-        }),
+        })
+        .optional(),
       body('prestacao.itemLista', 'Deve ser uma string de até 5 caracteres')
         .isString().isLength({ max: 5 }),
       body('prestacao.codCnae', 'Deve ser uma string de até 20 caracteres')
-        .isString().isLength({ max: 20 }),
+        .isString().isLength({ max: 20 }).optional(),
       body('prestacao.codServico', 'Deve ser uma string de até 20 caracteres')
-        .isString().isLength({ max: 20 }),
+        .isString().isLength({ max: 20 }).optional(),
       body('prestacao.codNBS', 'Deve ser uma string de até 9 caracteres')
-        .isString().isLength({ max: 9 }),
+        .isString().isLength({ max: 9 }).optional(),
       body('prestacao.discriminacao', 'Deve ser uma string de até 2000 caracteres')
         .isString().isLength({ max: 2000 }),
       body('prestacao.exigibilidadeISS', 'Deve ser um inteiro entre 1 e 7')
         .isInt({ min: 1, max: 7 }),
       body('prestacao.codTributMunicipio')
-        .isInt(),
+        .isInt().optional(),
       body('prestacao.numProcesso', 'Deve ser uma string de até 30 caracteres')
-        .isString().isLength({ max: 30 }),
+        .isString().isLength({ max: 30 }).optional(),
       body('prestacao.regimeEspTribut')
-        .isInt({ min: 1, max: 6 }),
+        .isInt({ min: 1, max: 6 }).optional(),
       body('prestacao.optanteSimplesNacional', 'valor deve ser true ou false')
         .isBoolean(),
       body('prestacao.incentivoFiscal')
         .isBoolean(),
       body('tomador.identificacaoTomador')
-        .isString().isLength({ max: 14 }),
+        .isString().isLength({ max: 14 }).optional(),
       body('tomador.nif')
-        .isString().isLength({ max: 40 }),
+        .isString().isLength({ max: 40 }).optional(),
       body('tomador.nomeRazaoTomador')
-        .isString().isLength({ max: 150 }),
+        .isString().isLength({ max: 150 }).optional(),
       body('tomador.logEnd')
-        .isString().isLength({ max: 125 }),
+        .isString().isLength({ max: 125 }).optional(),
       body('tomador.nif')
-        .isString().isLength({ max: 40 }),
+        .isString().isLength({ max: 40 }).optional(),
       body('tomador.numEnd')
-        .isString().isLength({ max: 10 }),
-      body('tomador.compEnd')
-        .isString().isLength({ max: 60 }),
+        .isString().isLength({ max: 10 })
+        .optional(),
+      body('tomador.compEnd').optional()
+        .isString().isLength({ max: 60 })
+        .optional(),
       body('tomador.bairroEnd')
-        .isString().isLength({ max: 60 }),
+        .isString().isLength({ max: 60 }).optional(),
       body('tomador.cidadeEnd')
-        .isInt(),
+        .isInt().optional(),
       body('tomador.estadoEnd')
-        .isString().isLength({ max: 2 }),
+        .isString().isLength({ max: 2 }).optional(),
       body('tomador.paisEnd')
-        .isInt(),
+        .isInt().optional(),
       body('tomador.cepEnd')
-        .isString().isLength({ max: 8 }),
+        .isString().isLength({ max: 8 }).optional(),
       body('tomador.email')
-        .isString().isEmail(),
+        .isString().isEmail().optional(),
       body('tomador.tel')
-        .isString().isLength({ max: 20 }),
+        .isString().isLength({ max: 20 }).optional(),
       body('intermediario.identificacaoIntermed')
-        .isString().isLength({ max: 14 }),
+        .isString().isLength({ max: 14 }).optional(),
       body('intermediario.nomeRazaoIntermed')
-        .isString().isLength({ max: 150 }),
+        .isString().isLength({ max: 150 }).optional(),
       body('intermediario.cidadeIntermed')
-        .isInt(),
+        .isInt().optional(),
       body('constCivil.codObra')
-        .isString().isLength({ max: 30 }),
+        .isString().isLength({ max: 30 }).optional(),
       body('constCivil.art')
-        .isString().isLength({ max: 30 }),
+        .isString().isLength({ max: 30 }).optional(),
     ],
   },
 
