5efb93cb68c068114b6e847245b43fca6fedd6bf
Tiago Loriato <tloriatosimoes@gmail.com>
2019-07-09 22:17:42 -0300

    API: noLogs for Seed Data

--- api/src/data/index.js
+++ api/src/data/index.js
@@ -8,26 +8,21 @@ export default function (models) {
   console.log('SETUP - Starting to populate tables with Initial Data');
 
   return new Promise(async (resolve, reject) => {
+    const noLogs = { log: () => {} };
     try {
-      await fixtures.loadFile(`${__dirname}/estado/estados.js`, models, { log: () => {} });
-      await fixtures.loadFile(`${__dirname}/regiao/regioes.js`, models, { log: () => {} });
+      await fixtures.loadFile(`${__dirname}/estado/estados.js`, models, noLogs);
+      await fixtures.loadFile(`${__dirname}/regiao/regioes.js`, models, noLogs);
       if (process.env.NODE_ENV === 'production') {
-        fixtures.loadFile(`${__dirname}/municipio/municipios.js`, models, { log: () => {} });
+        fixtures.loadFile(`${__dirname}/municipio/municipios.js`, models, noLogs);
       } else {
-        await fixtures.loadFile(`${__dirname}/municipio/municipiosSmall.js`, models, {
-          log: () => {},
-        });
-        await fixtures.loadFile(`${__dirname}/prefeitura/prefeituras.js`, models, {
-          log: () => {},
-        });
-        await fixtures.loadFile(`${__dirname}/empresa/empresas.js`, models, { log: () => {} });
-        await fixtures.loadFile(`${__dirname}/user/users.js`, models, { log: () => {} });
-        await fixtures.loadFile(`${__dirname}/client/clients.js`, models, { log: () => {} });
-        await fixtures.loadFile(`${__dirname}/authorizationCode/authorizationCodes.js`, models, {
-          log: () => {},
-        });
+        await fixtures.loadFile(`${__dirname}/municipio/municipiosSmall.js`, models, noLogs);
+        await fixtures.loadFile(`${__dirname}/prefeitura/prefeituras.js`, models, noLogs);
+        await fixtures.loadFile(`${__dirname}/empresa/empresas.js`, models, noLogs);
+        await fixtures.loadFile(`${__dirname}/user/users.js`, models, noLogs);
+        await fixtures.loadFile(`${__dirname}/client/clients.js`, models, noLogs);
+        await fixtures.loadFile(`${__dirname}/authorizationCode/authorizationCodes.js`, models, noLogs);
         // await generateInvoices(500);
-        await generateBlocks(4);
+        // await generateBlocks(4);
       }
       process.emit('dataLoaded');
       resolve();
