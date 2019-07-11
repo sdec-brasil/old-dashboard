0e5987efe4a8d792e8f72792fbe45ecd2887fd1f
David Beyda <dbeyda@globo.com>
2019-07-11 20:20:34 -0300

    Adapting Companies endpoints to new ErrorHandling structure

--- api/src/controllers/Companies.js
+++ api/src/controllers/Companies.js
@@ -2,18 +2,26 @@
 import service from '../services/companies';
 
 export default class CompanyController {
-  async get(req, res) {
-    const response = await service.listCompanies(req);
-    res.status(response.code).send(response.data);
+  async get(req, res, next) {
+    try {
+      const response = await service.listCompanies(req);
+      res.status(response.code).send(response.data);
+    } catch (err) {
+      next(err);
+    }
   }
 
   async post(req, res) {
     throw new Error('Not implemented');
   }
 
-  async getById(req, res) {
-    const response = await service.getCompany(req);
-    res.status(response.code).send(response.data);
+  async getById(req, res, next) {
+    try {
+      const response = await service.getCompany(req);
+      res.status(response.code).send(response.data);
+    } catch (err) {
+      next(err);
+    }
   }
 
   async update(req, res) {
--- api/src/services/companies/index.js
+++ api/src/services/companies/index.js
@@ -1,60 +1,61 @@
 import ResponseList from '../../utils/response';
 import { limitSettings } from '../../config/config';
 import models from '../../models';
-import { treatNestedFilters, customErr } from '../../utils';
+import { treatNestedFilters, errors } from '../../utils';
 
 const sqs = require('sequelize-querystring');
 
-const listCompanies = async req => new Promise(async (resolve) => {
+const listCompanies = async (req) => {
   const sq = sqs.withSymbolicOps(models.Sequelize, {});
-  let filter = req.query.filter ? req.query.filter : '';
+  const filter = req.query.filter ? req.query.filter : '';
 
-  if (!filter.includes('block.block_datetime')) {
-    if (filter.length === 0) {
-      filter += `block.block_datetime lte ${((new Date()).toISOString())}`;
-    } else {
-      filter += `, block.block_datetime lte ${((new Date()).toISOString())}`;
-    }
-  }
+  // TODO: for this to work, the empresa model needs a key to the block it was registered on
+  // if (!filter.includes('block.block_datetime')) {
+  //   if (filter.length === 0) {
+  //     filter += `block.block_datetime lte ${((new Date()).toISOString())}`;
+  //   } else {
+  //     filter += `, block.block_datetime lte ${((new Date()).toISOString())}`;
+  //   }
+  // }
   let where = null;
   try {
     where = sq.find(filter);
   } catch (err) {
-    resolve(customErr.BadFilterError);
-    throw err;
+    throw new errors.BadFilterError();
   }
   treatNestedFilters(filter, where);
 
-  models.empresa.findAndCountAll({
+  return models.empresa.findAndCountAll({
     offset: parseInt(req.query.offset, 10) || 0,
     limit: parseInt(req.query.limit, 10) || limitSettings.invoice.get,
     where,
     order: req.query.sort ? sq.sort(req.query.sort) : [],
   }).then((results) => {
     const response = new ResponseList(req, results, filter);
-    resolve({ code: 200, data: response.value() });
+    return { code: 200, data: response.value() };
   }).catch((err) => {
-    resolve({ code: 500, data: { error: customErr.formatErr(err) } });
     throw err;
   });
-});
+};
 
-const getCompany = async req => new Promise(async (resolve) => {
+const getCompany = async req =>
   // search by cnpj
-  let company = await models.empresa.findByPk(req.params.id);
-  if (company) {
-    resolve({ code: 200, data: company });
-  } else {
-    // search by public address
-    company = await models.empresa.findOne({
-      where: { enderecoBlockchain: req.params.id },
+  models.empresa.findByPk(req.params.id)
+    .then((companyByCnpj) => {
+      if (companyByCnpj) {
+        return { code: 200, data: companyByCnpj };
+      }
+      // search by public address
+      return models.empresa.findOne({
+        where: { enderecoBlockchain: req.params.id },
+      })
+        .then((companyByAddress) => {
+          if (companyByAddress) {
+            return { code: 200, data: companyByAddress };
+          }
+          throw new errors.NotFoundError('Company', `CNPJ or blockchainAddress ${req.params.id}`);
+        });
     });
-    if (company) {
-      resolve({ code: 200, data: company });
-    }
-  }
-  resolve(customErr.NotFoundError);
-});
 
 
 export default {
